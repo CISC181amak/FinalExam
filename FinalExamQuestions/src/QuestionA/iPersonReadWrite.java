0b1fcde1e2ceaab1b2737b615e84eff88ebcee9e
alex <alex@128.4.241.82>
2015-12-18 19:01:44 -0500

    Final

---
+++ FinalExamQuestions/src/QuestionA/iPersonReadWrite.java
@@ -0,0 +1,15 @@
+package QuestionA;
+
+import java.util.Date;
+
+public interface iPersonReadWrite {
+
+	public void setFirstName(String firstName);
+	public void setMiddleName(String middleName);
+	public void setLastName(String lastName);
+	public void setDOB(Date DOB);
+	public void setAddress(String address);
+	public void setPhone(String phone_number);
+	public void setEmail(String email_address);
+	
+}
--- FinalExamQuestions/src/QuestionE/Student_Test.java
+++ FinalExamQuestions/src/QuestionE/Student_Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -40,9 +41,19 @@ public class Student_Test {
 			e.printStackTrace();
 		}
 		Student stu1 = new Student("Bert", "Randall", "Gibbons", dBirthDate, "214 Labrador Lane", "302-893-6889", "bgibbons@udel.edu","MIS");
-
-
-		assertTrue(1==1);
+		Student stu2 = new Student("A", "", "", new Date(), "", "", "", "");
+		Student stu3 = new Student("A", "", "", new Date(), "", "", "", "");
+		Student stu4 = new Student("A", "", "", new Date(), "", "", "", "");
+		Student stu5 = new Student("A", "", "", new Date(), "", "", "", "");
+		
+		HashMap hm = new HashMap();
+		hm.add(stu1);
+		hm.add(stu2);
+		hm.add(stu3);
+		hm.add(stu4);
+		hm.add(stu5);
+		
+		AssertEquals(hm.size(), 5);
 	}
 
 }
